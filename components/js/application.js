a6dee468b572070cf29a9425a1a59a71cbf3bcb8
Thomas Wicker <design@thomaswicker.com>
2014-10-10 08:46:54 -0600

    edits to JS for scrolling and minor SCSS edits

--- components/js/application.js
+++ components/js/application.js
@@ -9,11 +9,23 @@ $(document).ready(function () {
 	  $(this).attr("src", newSrc); 
 	});
 
+	$('#right-off-canvas-toggle').mouseover(function(){
+	  var newSrc = $(this).attr("src").replace("btns/notes_btn_up.png", "btns/notes_btn_over.png");
+	  $(this).attr("src", newSrc); 
+	});
+	$('#right-off-canvas-toggle').mouseout(function(){
+	  var newSrc = $(this).attr("src").replace("btns/notes_btn_over.png", "btns/notes_btn_up.png");
+	  $(this).attr("src", newSrc); 
+	});
+
 	//Scrollable Div Plugin Initialization
 	$("section#scrollableNav").smoothDivScroll({
-		mousewheelScrolling: "allDirections",
 		manualContinuousScrolling: true,
 		touchScrolling: true,
 	});
 
+	$('.right-off-canvas-toggle').click(function() {
+    $('.notes').slideToggle('fast');
+	    return false;
+	});
 });
